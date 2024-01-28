2f7bebeb3a75476619d6fbe289c3208d72197c46
Alejandro Armas <alejandroarmas152@gmail.com>
2024-01-28 12:06:35 -0300

    carlitox lvlv4

---
+++ scenes/levels/Level4.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+@onready var Progreso = %Progreso
+@onready var Spawn = %Spawn
+@onready var Jugadores = %Jugadores
+@onready var Jefe = %Carlitox
---
+++ scenes/prefabs/carlitox.gd
@@ -0,0 +1,39 @@
+extends RigidBody2D
+
+var ratas = []
+
+@export var SPEED = 200.0
+
+var velocity:Vector2 = Vector2(-SPEED,-SPEED)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	apply_impulse(velocity)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#add_constant_central_force(velocity)
+	linear_velocity=velocity
+	
+
+
+func _on_body_entered(body):
+	if(body.name == 'borde_sup'):
+		#apply_impulse(Vector2(-SPEED,SPEED))
+		velocity[1] = SPEED
+		#constant_force=(Vector2(SPEED,SPEED))
+	elif(body.name == 'borde_inf'):
+		velocity[1] = -SPEED
+	elif(body.name == 'borde_izq'):
+		velocity[0] = SPEED
+	elif(body.name == 'borde_der'):
+		velocity[0] = -SPEED
+			
+		
+	
+
+
+func acelerar():
+	
+	$AnimationPlayer.play('mover_las_patas')
+
