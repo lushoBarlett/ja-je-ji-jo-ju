1e4d7bc5ee98bedb8acdd510ec6356f45589db61
Alejandro Armas <alejandroarmas152@gmail.com>
2024-01-28 14:49:24 -0300

    lvl 4 musica dinamicaa

--- scenes/prefabs/carlitox.gd
+++ scenes/prefabs/carlitox.gd
@@ -2,8 +2,14 @@ extends RigidBody2D
 
 var ratas = []
 var GameFunction
+var veces = 0
 
 @export var SPEED = 200.0
+
+@export var DRUMS: AudioStreamPlayer
+@export var CHORDS: AudioStreamPlayer
+@export var MELO1: AudioStreamPlayer
+@export var MELO2: AudioStreamPlayer
 
 var velocity:Vector2 = Vector2(-SPEED,-SPEED)
 # Called when the node enters the scene tree for the first time.
@@ -37,4 +43,17 @@ func acelerar():
 	SPEED += 20
 	$AnimationPlayer.play('mover_las_patas')
 	$sfx_jojojo.play()
+	veces +=1
 
+
+
+func _on_drums_lupie():
+	if veces >= 1 and not CHORDS.playing:
+		CHORDS.play()
+		print('entran chords')
+	if veces >= 3 and not MELO1.playing:
+		MELO1.play() 
+		print('entra melo1')
+	if veces >= 5 and not MELO2.playing:
+		MELO2.play()
+		print('entra melo2')
