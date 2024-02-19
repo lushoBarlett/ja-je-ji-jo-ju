1ff2f5c0549cdb9d03b8bdddb6fc9549b9f1b6ab
Alejandro Armas <alejandroarmas152@gmail.com>
2024-02-19 15:17:54 -0300

    Mejora el disparo de Jojo basando cd en tiempo y no en 1 frame , cantidad de disparos dinamica segun ratas vivas

--- scripts/objects/jojo.gd
+++ scripts/objects/jojo.gd
@@ -12,39 +12,35 @@ extends RigidBody2D
 var GameFunction = "Enemy"
 
 var ratas := []
-var rata: CharacterBody2D
+var rata : CharacterBody2D
 var tiros := 0
 var veces := 0
-var colisione := false
-var direccion_patas: Vector2
-var direccion_rata: Vector2
+var time_since_shot := 0.0
+var direccion_patas : Vector2
+var direccion_rata : Vector2
 var velocity: Vector2 = Vector2(-SPEED,-SPEED)
 
 func _ready():
+	linear_velocity=velocity
 	$CuerpoBoca.visible = false
-	linear_velocity = velocity
 
-func _physics_process(_delta):
-	if not $AnimationPlayer.is_playing():
-		return
 
-	rata = ratas.pick_random()
-	direccion_patas = Vector2(0,1).rotated($Patas.rotation).normalized()
-	direccion_rata = $Patas.global_position.direction_to(rata.global_position).normalized()
+func _process(_delta):
+	pass
+	
+func _physics_process(delta):
+	if $AnimationPlayer.is_playing():
+		rata = ratas.pick_random()
+		direccion_patas = Vector2(0,1).rotated($Patas.rotation).normalized()
+		direccion_rata = $Patas.global_position.direction_to(rata.global_position).normalized()
+		if(direccion_patas.dot(direccion_rata) >= 0.99):
+			if(time_since_shot > 0.1 and tiros <= (ratas.size() + 1)): 
+				tiros +=1
+				disparar(rata.global_position)
+				time_since_shot = 0.0
+		time_since_shot += delta
 
-	if direccion_patas.dot(direccion_rata) < 0.99:
-		colisione = false
-		return
 
-	if colisione:
-		print('col pero no disparo')
-		#La idea es que no salgan dos disparos ultra pegados
-	else:
-		tiros += 1
-		if tiros <= 3: #arbitrario, lo haria basado en la cant de players
-			disparar(rata.global_position)
-
-	colisione = true
 
 func _on_body_entered(body):
 	if(body.name == 'borde_sup'):
@@ -58,9 +54,8 @@ func _on_body_entered(body):
 
 	elif(body.name == 'borde_der'):
 		velocity[0] = -SPEED
-
-	linear_velocity = velocity
-
+	linear_velocity=velocity
+	
 func acelerar():
 	$AnimationPlayer.play('mover_las_patas')
 	$TimerFinAnimacion.start()
@@ -68,14 +63,15 @@ func acelerar():
 	$sfx_jojojo.play()
 	veces += 1
 
-func disparar(dirección: Vector2) -> void:
+func disparar(dir):
 	var pata: RigidBody2D = Pata.instantiate()
 	pata.global_position = $Patas.get_global_position()
-	pata.apuntar = pata.position.direction_to(dirección).normalized() * 300
+	pata.apuntar = pata.position.direction_to(dir).normalized() * 300
 	pata.rotation = $Patas.rotation
 	get_tree().get_root().add_child(pata)
 
 func _on_drums_lupie():
+
 	if veces >= 1 and not Chords.playing:
 		Chords.play()
 	if veces >= 3 and not Melody1.playing:
@@ -83,6 +79,9 @@ func _on_drums_lupie():
 	if veces >= 5 and not Melody2.playing:
 		Melody2.play()
 
+
+
 func _on_timer_fin_animacion_timeout():
 	tiros = 0
+
 
