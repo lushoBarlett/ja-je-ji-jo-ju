c2ce5334d2dc79a4c89b4187ba9021166c1733ae
Alejandro Armas <alejandroarmas152@gmail.com>
2024-02-23 18:36:17 -0300

    Edita scrips niveles, creacion dinamica de ratas/asignacion de skins

--- scripts/levels/level1.gd
+++ scripts/levels/level1.gd
@@ -5,13 +5,20 @@ extends Node2D
 @onready var Jugadores = %Jugadores
 @onready var Jefe = %JaJa
 @export var player_count : int
-@export var skins = []
+@export var skins := []
+var spawns : Array
+
 
 func _ready():
+	spawns.insert(0,null)
+	spawns.insert(1,null)
+	spawns.insert(2,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2])
+	spawns.insert(3,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2,$SpawnRatas/Spawn5])
+	spawns.insert(4,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2,$SpawnRatas/Spawn3,$SpawnRatas/Spawn4])
 	for i in range(player_count):
 		var rata = preload("res://scenes/prefabs/Rata.tscn").instantiate()
 		rata.name = 'Rata' + str(i+1)
 		rata.skin = skins[i]
-		rata.position = Vector2(1830,500)
+		rata.position = spawns[player_count][i].position
 		rata.player = i+1
 		Jugadores.add_child(rata)
--- scripts/levels/level4.gd
+++ scripts/levels/level4.gd
@@ -5,12 +5,19 @@ extends Node2D
 @onready var Jugadores = %Jugadores
 @onready var Jefe = %JoJo
 @export var player_count : int
+@export var skins = []
+var spawns : Array
 
 func _ready():
-	if (player_count >= 3) :
-		var skin3 = preload("res://assets/rata.png")
-		var rata3 = preload("res://scenes/prefabs/Rata.tscn").instantiate()
-		rata3.position = Vector2(1830,500)
-		rata3.player = 3
-		rata3.skin = skin3
-		Jugadores.add_child(rata3)
+	spawns.insert(0,null)
+	spawns.insert(1,null)
+	spawns.insert(2,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2])
+	spawns.insert(3,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2,$SpawnRatas/Spawn5])
+	spawns.insert(4,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2,$SpawnRatas/Spawn3,$SpawnRatas/Spawn4])
+	for i in range(player_count):
+		var rata = preload("res://scenes/prefabs/Rata.tscn").instantiate()
+		rata.name = 'Rata' + str(i+1)
+		rata.skin = skins[i]
+		rata.position = spawns[player_count][i].position
+		rata.player = i+1
+		Jugadores.add_child(rata)
