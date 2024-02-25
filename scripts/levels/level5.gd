0b216658ec2071b38b8540cc3305d3c9731c188e
Alejandro Armas <alejandroarmas152@gmail.com>
2024-02-25 18:43:45 -0300

    Cambia script niveles para soportar nuevo sistema de player/skins

--- scripts/levels/level5.gd
+++ scripts/levels/level5.gd
@@ -4,20 +4,21 @@ extends Node2D
 @onready var Spawn = %Spawn
 @onready var Jugadores = %Jugadores
 @onready var Jefe = %JuJu
-@export var player_count : int
-@export var skins = []
+@export var players := []
 var spawns : Array
 
 func _ready():
 	spawns.insert(0,null)
-	spawns.insert(1,null)
+	spawns.insert(1,[$SpawnRatas/Spawn1])
 	spawns.insert(2,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2])
 	spawns.insert(3,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2,$SpawnRatas/Spawn5])
 	spawns.insert(4,[$SpawnRatas/Spawn1,$SpawnRatas/Spawn2,$SpawnRatas/Spawn3,$SpawnRatas/Spawn4])
-	for i in range(player_count):
+	var i := 1
+	for player in players:
 		var rata = preload("res://scenes/prefabs/Rata.tscn").instantiate()
-		rata.name = 'Rata' + str(i+1)
-		rata.skin = skins[i]
-		rata.position = spawns[player_count][i].position
-		rata.player = i+1
+		rata.name = 'Rata' + str(i)
+		rata.skin = player['skin']
+		rata.position = spawns[players.size()][i-1].position
+		rata.player = player['controls']
 		Jugadores.add_child(rata)
+		i += 1
